b4c64c32a200c222f93f4fbada4a1000082517e5
Joakim Kartveit <joakimkartveit@gmail.com>
2020-02-27 12:52:54 +0100

    Dropper hele cio går over til apache

--- pale-2-common-rest-sts/build.gradle.kts
+++ pale-2-common-rest-sts/build.gradle.kts
@@ -1,4 +1,4 @@
-val ktorVersion = "1.3.0"
+val ktorVersion = "1.3.1"
 val jacksonVersion = "2.9.8"
 
 repositories {
@@ -12,7 +12,7 @@ plugins {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
     implementation("io.ktor:ktor-client-auth-jvm:$ktorVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
