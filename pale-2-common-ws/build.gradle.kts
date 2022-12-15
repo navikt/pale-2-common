1d982b6496efa17dcca2ee54e1da84b251e4ca69
Tia Firing <tia.firing@nav.no>
2022-12-15 09:17:09 +0100

    ekskluderer velocity

--- pale-2-common-ws/build.gradle.kts
+++ pale-2-common-ws/build.gradle.kts
@@ -10,7 +10,9 @@ dependencies {
     api("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
     api("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
     api("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
-    api("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
+    api("org.apache.cxf:cxf-rt-ws-security:$cxfVersion") {
+        exclude(group = "org.apache.velocity", module = "velocity")
+    }
 }
 
 publishing {
